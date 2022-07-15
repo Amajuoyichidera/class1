269437431cec7e14502df90f88425647fcd98490
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-07-15 15:55:39 -0700

    first version

---
+++ script.js
@@ -0,0 +1,11 @@
+let one = document.querySelector('.ist')
+let two = document.querySelector('.sec')
+
+one.addEventListener('click', function () {
+    divElement.classList.remove('hide')
+})
+
+two.addEventListener('click', function () {
+    divElement.classList.add('hide')
+})
+
